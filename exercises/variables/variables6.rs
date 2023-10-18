833882b6a931ca7975f2ff1c0691a57100c32b20
jinzbQAQ <3408699065@qq.com>
2023-10-19 01:02:43 +0800

    update

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,12 +1,10 @@
-// I AM NOT DONE
-
 
 // variables6.rs
 //
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-const NUMBER = 3;
+const NUMBER:i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
