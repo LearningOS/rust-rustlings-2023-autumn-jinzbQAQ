f8cdbd6559635f95ba3126fd3c72b199547aa6b8
jinzbQAQ <3408699065@qq.com>
2023-10-22 13:26:45 +0800

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,18 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(i32, i32, i32),
+    Echo(String),
+    Point {
+        x : i32, 
+        y : i32
+    },
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -43,6 +51,13 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message{
+            Message::ChangeColor(a,b,c) =>   self.color=(a.try_into().unwrap(),b.try_into().unwrap(),c.try_into().unwrap()),
+            Message::Echo(a) =>self.message=String::from("hello world"),
+            Message::Move(Point { x, y })=> self.position=Point{x,y},
+            Message::Quit => self.quit=true,
+            _ => println!("不ok"),
+        };
     }
 }
 
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,55 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut x=input.to_string();
+    let mut j:u32=0;
+    for i in x.chars(){
+        if i==' '{
+           j=j+1; 
+        }
+        else {
+            break;
+        }
+    }
+    while(j!=0){
+        x.remove(0);
+        j=j-1;
+    }
+    let mut m:u32=(input.len()-1).try_into().unwrap();
+    while(input.chars().nth(m.try_into().unwrap()) == Some(' ') ){
+            m=m-1;
+    }
+    while(m!=(input.len()-1).try_into().unwrap()){
+        m=m+1;
+        x.pop();
+    }
+    
+    // while(x[0]==" "){
+    //    x.remove(0);
+    // }
+    // let mut i = x.len()-1;
+    // while(x[i]==" "){
+      
+    //     x.pop();
+    //     i=x.len();
+    // }
+    x
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut x=input.to_string();
+    x.push_str(" world!");
+    x
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let x=input.to_string();
+    x.replace("cars","balloons")
 }
 
 #[cfg(test)]
